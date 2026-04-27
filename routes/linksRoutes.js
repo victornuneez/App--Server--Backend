ed0f8ff9e9ee7a1e5db1aa43705fccf5ea243cb9
Victor Nunez <tu_correo@gmail.com>
2026-04-27 18:58:54 -0300

    feat: agregar nuevas rutas de anhadir comentarios, votos y observar todos los datos de un enlace

--- routes/linksRoutes.js
+++ routes/linksRoutes.js
@@ -1,9 +1,12 @@
 import { Router } from "express";
-import { createLinks, createTag } from "../controller/linksController.js";
+import { createLinks, createTag, addComment, addVote, linkDetail } from "../controller/linksController.js";
 
 const router = Router();
 
-router.post('/', createLinks)
-router.post('/tag', createTag)
+router.post('/', createLinks);
+router.post('/tag', createTag);
+router.patch('/comment/:id', addComment);
+router.patch('/vote/:id', addVote);
+router.get('/details/:id', linkDetail);
 
 export default router;
