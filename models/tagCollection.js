a269b85a49eebb24ce76aa994f19ac77db1433a3
Victor Nunez <tu_correo@gmail.com>
2026-05-04 17:46:54 -0300

    refactor: agregar una nueva configuracion al campo de nombre de las etiquetas

--- models/tagCollection.js
+++ models/tagCollection.js
@@ -1,12 +1,12 @@
 import mongoose from 'mongoose';
 import { timeStamp } from 'node:console';
 
-const tag = new mongoose.Schema({
-    tag : { type: String, required: true, unique: true, trim: true },
+const tagSchema = new mongoose.Schema({
+    name : { type: String, required: true, unique: true, trim: true },
 }, {
     timestamps: true,
     versionKey: false  
 });
 
-const Tag = mongoose.model('Tag', tag);
+const Tag = mongoose.model('Tag', tagSchema);
 export default Tag;
