65f63913e852c797f3963b73ccdb14d55eb66234
Victor Nunez <tu_correo@gmail.com>
2026-05-04 17:42:37 -0300

    feat: agregar funciones de la app

---
+++ controller/appController.js
@@ -0,0 +1,43 @@
+import Link from '../models/linkCollection.js';
+import Tag from '../models/tagCollection.js';
+
+const getLinkDetails = async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        const link = await Link.findById(id).populate('tag','name');
+
+        if(!link) {
+            return res.status(404).json({ message: "Recurso no encontrado" });
+        }
+
+        res.status(200).json(link);
+    
+    } catch (error) {
+        res.status(500).json({ message: "Error en el servidor", error: error.message });
+    }
+};
+
+const filterTags = async (req, res) => {
+    const { tag } = req.query;
+    let filter = {};
+
+    try {
+        if (tag && tag !== "Todos") {
+            const tagDoc = await Tag.findOne({ name : tag });
+
+            if(tagDoc) {
+                filter = { tag: tagDoc._id }; // armamos el filtro por id.
+            }
+        }
+
+        // Si tag no viene en la URL, filter queda como un objeto vacio y Mongoose devuelve todo.
+        const links = await Link.find(filter).populate('tag','name');
+        res.status(200).json(links);
+
+    } catch (error) {
+        return res.status(500).json({ message: "Error en el servidor", error: error.message });
+    }
+};
+
+export { getLinkDetails, filterTags };
