fdb48d6349850cf601ce1d22e22ba28396247b16
Victor Nunez <tu_correo@gmail.com>
2026-05-06 20:14:46 -0300

    commit feat: agregar ruta de funcion para obtener todas las etiquetas de la base de datos

--- routes/appRoutes.js
+++ routes/appRoutes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import { getLinkDetails, filterTags } from "../controller/appController.js";
+import { getLinkDetails, filterTags, getTags } from "../controller/appController.js";
+import { get } from "mongoose";
 
 const router = Router();
 
 router.get('/links', filterTags);
 router.get('/details/:id', getLinkDetails);
+router.get('/tags', getTags);
 
 export default router;
