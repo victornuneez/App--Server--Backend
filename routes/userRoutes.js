01fad15a7dd70eeda259ae7cfec79a169676f7d8
Victor Nunez <tu_correo@gmail.com>
2026-05-08 19:31:23 -0300

    feat: definir nuevas rutas de editar un link y eliminar un link

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
-import { createLinks, createTag, addComment, addVote } from "../controller/userController.js";
+import { createLinks, createTag, updateLink, addComment, addVote, deleteLink } from "../controller/userController.js";
 
 const router = Router();
 
 router.post('/create', createLinks);
 router.post('/tag', createTag);
+router.put('/update/:id', updateLink);
 router.patch('/comment/:id', addComment);
 router.patch('/vote/:id', addVote);
+router.delete('/delete/:id', deleteLink);
 
 export default router;
